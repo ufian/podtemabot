c4b72b4b4106d8de26c4b82ab8d3da874c22970f
ufian <ufian@ya.ru>
2020-02-16 14:02:38 -0800

    Add support question in the middle

--- podtema/question_handler.py
+++ podtema/question_handler.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
 
 import datetime as dt
+import re
 
 from podtema.model import Messages
 
+QUESTION_PATTERN = re.compile(r'(\w{2,})\?')
 
 def handler(msg):
     text = msg['text'].strip()
     msg_from = msg['from']
     user = msg_from.get('username', msg_from.get('first_name'))
 
-    if not text.endswith("?"):
+    words = QUESTION_PATTERN.findall(text)
+
+    if not words:
         return "Непонятно без вопроса"
 
     if Messages.objects(user=user, date__gt=dt.datetime.now() - dt.timedelta(days=1)).count() >= 3:
